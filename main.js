77584a2b44841193d72146bbbc822331cc69b2b0
wes-mitchell <wmdrums@gmail.com>
2022-02-17 10:24:33 -0600

    Another practice rounds of files saved for commit

---
+++ main.js
@@ -0,0 +1,51 @@
+const createChassis = () => {
+  const newChassisObject = {}
+  
+  return newChassisObject
+}
+
+const addBody = (chassisObject) => {
+  chassisObject.body = "Fever"
+
+  return chassisObject
+}
+
+const addWheels = (chassisObject) => {
+  chassisObject.wheels = 4
+
+  return chassisObject
+}
+
+const addEngine = (chassisObject) => {
+  chassisObject.engine = "4.8L"
+
+  return chassisObject
+}
+
+const addSteeringWheel = (chassisObject) => {
+  chassisObject.steeringWheel = "Tilting Steering Wheel"
+
+  return chassisObject
+}
+
+const addDriveTrain = (chassisObject) => {
+  chassisObject.driveTrain = "Two wheel drive"
+
+  return chassisObject
+}
+
+
+const buildCar = () => {
+  const chassis = createChassis()
+  const chassisBody = addBody(chassis)
+  const chassisWheels = addWheels(chassisBody)
+  const chassisEngine = addEngine(chassisWheels)
+  const chassisSteeringWheel = addSteeringWheel(chassisEngine)
+  const chassisDriveTrain = addDriveTrain(chassisSteeringWheel)
+
+  return chassisDriveTrain
+}
+
+const car1 = buildCar()
+
+console.log(car1)
